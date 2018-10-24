61eb9500e10440a7815d8561746af59c4b857532
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-10-23 20:26:30 -0700

    includes model object and initializer

---
+++ LyricFinder/LyricFinder/Song.h
@@ -0,0 +1,24 @@
+//
+//  Song.h
+//  LyricFinder
+//
+//  Created by Jonathan T. Miles on 10/23/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Song : NSObject
+
+- (instancetype)initWithTitle:(NSString *)title artist:(NSString *)artist lyrics:(NSString *)lyrics rating:(int)rating;
+
+@property NSString *title;
+@property NSString *artist;
+@property NSString *lyrics;
+@property int rating;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LyricFinder/LyricFinder/Song.m
@@ -0,0 +1,25 @@
+//
+//  Song.m
+//  LyricFinder
+//
+//  Created by Jonathan T. Miles on 10/23/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+#import "Song.h"
+
+@implementation Song
+
+- (instancetype)initWithTitle:(NSString *)title artist:(NSString *)artist lyrics:(NSString *)lyrics rating:(int)rating
+{
+    self = [super init];
+    if (self) {
+        _title = title;
+        _artist = artist;
+        _lyrics = lyrics;
+        _rating = rating;
+    }
+    return self;
+}
+
+@end
